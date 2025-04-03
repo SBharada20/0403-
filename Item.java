d97fce0d5faff930d744d55387afce7e4591cb63
Tomoaki HARADA <harada.sb20@gmail.com>
2025-04-03 14:16:57 +0900

    Item.javaを追加しました

---
+++ Item.java
@@ -0,0 +1,30 @@
+package pla9_10;
+
+public class Item {
+	private String Name;
+	private int Price;
+	private int Weight;
+	
+	public String getName() {
+		return this.Name;
+	}
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+	
+	public int getPrice() {
+		return this.Price;
+	}
+	public void setPrice(int Price) {
+		this.Price = Price;
+	}
+	
+	public int getWeight() {
+		return this.Weight;
+	}
+	public void setWeight(int Weight) {
+		this.Weight = Weight;
+	}
+	
+
+}
